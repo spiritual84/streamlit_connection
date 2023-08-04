c364c58cc6a4760e6d4ab1f9341e5c05f403f2f3
Yong Sheng Low <spiritual@wallwisher.com>
2023-08-04 22:17:07 +0800

    add map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from re import L
 from typing import Tuple
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -6,6 +7,8 @@ import streamlit as st
 import pandas as pd
 from streamlit.connections import ExperimentalBaseConnection
 import requests
+import folium
+from streamlit_folium import st_folium, folium_static
 
 class sgWeatherConnection(ExperimentalBaseConnection["sgWeatherConnection"]):
     def _connect(self, **kwargs) -> "sgWeatherConnection":
@@ -15,6 +18,10 @@ class sgWeatherConnection(ExperimentalBaseConnection["sgWeatherConnection"]):
         resp = requests.get(url="https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time="+r_date.strftime("%Y-%m-%dT%H:%M:%S"))
         data = resp.json()
         returnFrame = pd.DataFrame.from_dict(data["items"][0]["forecasts"])
+        returnFrame.set_index('area')
+        posDF = pd.DataFrame.from_dict(data["area_metadata"])
+        posDF.set_index('name')
+        returnFrame = pd.concat([returnFrame,posDF],axis=1)
         startDate = datetime.strptime(data["items"][0]["valid_period"]["start"][:-6],"%Y-%m-%dT%H:%M:%S")
         endDate = datetime.strptime(data["items"][0]["valid_period"]["end"][:-6],"%Y-%m-%dT%H:%M:%S")
         return (startDate,endDate,returnFrame)
@@ -36,4 +43,35 @@ conn = st.experimental_connection('sgweather', type=sgWeatherConnection)
 (startDate,endDate,weatherData) = conn.query(d_time)
 
 st.write(startDate.strftime("%d/%m/%Y, %H:%M:%S")+" to "+endDate.strftime("%d/%m/%Y, %H:%M:%S"))
-st.dataframe(data=weatherData,use_container_width=True,hide_index=True)
+#st.dataframe(data=weatherData,use_container_width=True,hide_index=True)
+m = folium.Map(location=[1.352083,103.819839],zoom_start = 11)
+for index, row in weatherData.iterrows():
+    if row['forecast'] == 'Partly Cloudy (Night)':
+        set_icon = "fa-cloud-moon"
+        set_color = "black"
+    elif row['forecast'] == 'Partly Cloudy (Day)':
+        set_icon = "fa-cloud-sun"
+        set_color = "blue"
+    elif row['forecast'] == 'Light Rain':
+        set_icon = "fa-cloud-rain"
+        set_color = "blue"
+    elif row['forecast'] == 'Light Showers':
+        set_icon = "fa-cloud-rain"
+        set_color = "blue"
+    elif row['forecast'] == 'Thundery Showers':
+        set_icon = "fa-cloud-showers-heavy"
+        set_color = "grey"
+    elif row['forecast'] == 'Cloudy':
+        set_icon = "fa-cloud"
+        set_color = "blue"
+    else:
+        set_icon = "x-mark-to-slot"
+        set_color = "red"
+
+    folium.Marker([row['label_location']['latitude'], row['label_location']['longitude']],
+                    popup="<strong>"+row['area']+"</strong><br/>"+row['forecast'],
+                    icon=folium.Icon(color = set_color, icon=set_icon, prefix='fa')
+                  ).add_to(m)
+folium_static(m)
+
+st.dataframe(data=weatherData[["area","forecast"]],use_container_width=True,hide_index=True)
