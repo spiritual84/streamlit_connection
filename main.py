6da83e28bc617e89fd199ec1c93caecaa9efe8a7
Yong Sheng Low <spiritual@wallwisher.com>
2023-08-04 23:45:22 +0800

    fix some icons

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ for index, row in weatherData.iterrows():
     elif row['forecast'] == 'Fair & Warm':
         set_icon = "fa-sun"
         set_color = "orange"
-    elif row['forecast'] == 'Fair Day':
+    elif row['forecast'] == 'Fair (Day)':
         set_icon = "fa-sun"
         set_color = "orange"
     elif row['forecast'] == 'Fair (Night)':
